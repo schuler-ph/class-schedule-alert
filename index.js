27f0f473ea05ba79f185864e06e41e88edb0bca3
Astr0nAT <astron.autnr@gmail.com>
2020-02-04 18:15:09 +0100

    Outputting school start time

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ let username = fs.readFileSync("username.txt").toString();
 let password = fs.readFileSync("password.txt").toString();
 
 const untis = new WebUntis(
-  "htl1-innsbruck",
+  "htl1-innsbruck", // Change this value to your own school id
   username,
   password,
-  "neilo.webuntis.com"
+  "neilo.webuntis.com" // Change this value to your own webuntis subdomain
 );
 
-let currentUntisDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-let finalOutput;
+const days = 1; // Amount of days in the future; also works with negative numbers and 0
 
-finalOutput = "Stundenplan vom " + currentUntisDate.getDate() + "." + (currentUntisDate.getMonth() + 1) + "." +
-    currentUntisDate.getFullYear() + ":\n";
+let currentUntisDate = new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * days);
+
+let finalOutputString = "Stundenplan vom " + currentUntisDate.getDate() + "." + (currentUntisDate.getMonth() + 1) +
+    "." + currentUntisDate.getFullYear() + ":\n\n";
 
 untis
   .login()
@@ -24,6 +25,7 @@ untis
     return untis.getOwnTimetableFor(currentUntisDate);
   })
   .then(timetable => {
+      let schoolStartTime = 2359;
       let schoolEndTime = 0;
     timetable.forEach(element => {
         let subjectName = element.su.values().next().value.name;
@@ -35,24 +37,30 @@ untis
                 output += subjectName + " fällt aus.";
                 break;
             case "irregular":
-                output += subjectName + " wird suppliert von " + element.te.values().next().value.longname + ".";
+                output += subjectName + " suppliert von " + element.te.values().next().value.longname + ".";
         }
         if(status === "irregular" || status === undefined){
+            if(element.startTime < schoolStartTime){
+                schoolStartTime = element.startTime;
+            }
             if(element.endTime > schoolEndTime){
                 schoolEndTime = element.endTime;
             }
         }
         if(output){
-            finalOutput += output + "\n";
+            finalOutputString += output + "\n";
         }
     });
 
     if(schoolEndTime !== 0){
+        schoolStartTime = schoolStartTime.toString();
+        schoolStartTime = schoolStartTime.charAt(0) + ":" + schoolStartTime.charAt(1) + schoolStartTime.charAt(2);
         schoolEndTime = schoolEndTime.toString();
         schoolEndTime = schoolEndTime.charAt(0) + schoolEndTime.charAt(1) + ":" + schoolEndTime.charAt(2) + schoolEndTime.charAt(3);
-        finalOutput += "\nSchulende um " + schoolEndTime + ".";
+        finalOutputString += "\nSchulstart um " + schoolStartTime + ".\n";
+        finalOutputString += "Schulende um " + schoolEndTime + ".";
     } else {
-        finalOutput += "\nKein Unterricht."
+        finalOutputString += "\nKein Unterricht."
     }
-    console.log(finalOutput);
+    console.log(finalOutputString);
   });
