bf5e6a1a435c7168c935d7af02dc73529159d70e
Astr0nAT <astron.autnr@gmail.com>
2020-02-05 19:20:04 +0100

    Added TODO for classes and starttimes

--- findTeacher.js
+++ findTeacher.js
@@ -12,6 +12,7 @@ const untis = new WebUntis(
     "neilo.webuntis.com" // Change this value to your own webuntis subdomain
 );
 
+// These are the class ids from the HTL Innsbruck TODO use untis.getClasses()
 let classes = "522 523 524 525 526 527 528 529 530 531 532 533 534 535 536 537 538 539 540 541 542 543 544 545 546 547 548 549 550 551 552 553 554 555 556 557 558 559 560 561 562 563 564 565 566 567 568 569 570 571 572 573 574 575 576 577 578 579 580 581 582 583 584 585 586 587 588 589 590 650 651 652 653 654 655 656 657 658 659 660 662 663 664 665"
 // 3AHWII = 526
 classes = classes.split(' ');
@@ -19,7 +20,8 @@ let currentUntisTime = new Date();
 currentUntisTime = currentUntisTime.getHours().toString() + currentUntisTime.getMinutes().toString();
 let currentLessonStarttime = 0;
 
-let starttimes = "800 850 955 1045 1140 1230 1320 1410 1515 1605 1655 1800 1845 1945 2030 2115 2200";
+// These are the times of the day a new lesson starts TODO use untis.getTimegrid()
+let starttimes = "800 850 955 1045 1140 1230 1320 1410 1515 1605 1655 1800 1845 1945 2030 2115";
 starttimes = starttimes.split(' ');
 
 starttimes.forEach(element => {
@@ -28,7 +30,7 @@ starttimes.forEach(element => {
     }
 });
 
-console.log(currentLessonStarttime = 955);
+console.log(currentLessonStarttime);
 
 let className = "";
 let teacherShort = "";
